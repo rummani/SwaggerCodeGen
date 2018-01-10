f3be7d32717cc76978448abc33dcc0cc24b44e43
rummani <33236365+rummani@users.noreply.github.com>
2018-01-10 11:28:18 -0700

    Update apigeelint.sh

--- apigeelint.sh
+++ apigeelint.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 node -v
- pwd
- ~/.nvm/versions/node/v6.11.5/bin/npm install apigeelint
- ./node_modules/apigeelint/main.js -s ./swaggerPetstore/apiproxy/ -f html.js
+ $ apigeelint -s swaggerPetstore/apiproxy -f table.js
+
